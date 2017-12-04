d207fbd496f76660eb22b5f9a53d3a6f3ab106c4
hathair5 <campbellyamane@gmail.com>
2017-12-04 15:27:36 -0800

    Timeline updated

--- js/map.js
+++ js/map.js
@@ -4,6 +4,7 @@ var position = [27.1959739, 78.02423269999997];
 function showGoogleMaps() {
 
     var latLng = new google.maps.LatLng(position[0], position[1]);
+    var center = new google.maps.LatLng(position[0], position[1]-.01);
 
     var mapOptions = {
         zoom: 16, // initialize zoom level - the max value is 21
@@ -11,7 +12,7 @@ function showGoogleMaps() {
         scaleControl: true, // allow users to zoom the Google Map
         mapTypeId: google.maps.MapTypeId.SATELLITE,
         disableDefaultUI: true,
-        center: latLng
+        center: center
     };
 
     map = new google.maps.Map(document.getElementById('map-canvas'),
