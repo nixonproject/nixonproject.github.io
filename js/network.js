2949c12516dc2d45e34b9ccf34f36fd183c534e4
dhuangg <danielhuang@g.ucla.edu>
2017-12-03 22:30:24 -0800

    network diagram - nixon centric draft 2

--- js/network.js
+++ js/network.js
@@ -1,51 +1,73 @@
-/**
- * Created by danielhuang on 12/3/17.
- */
-var width = 960,
-    height = 700
 
-var svg = d3.select("#five").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
 
-var force = d3.layout.force()
-    .gravity(.05)
-    .distance(100)
-    .charge(-100)
-    .size([width, height]);
+var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-d3.json("data/OctDecEdgeList-Weight.csv_1.json", function(json) {
-    force
-        .nodes(json.nodes)
-        .links(json.links)
-        .start();
+var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width/2, height/2));
 
-    var link = svg.selectAll(".link")
-        .data(json.links)
+d3.json("data/OctDecEdgeList-Weight.csv_1.json", function(error, graph) {
+    if (error) throw error;
+
+    var link = svg.append("g")
+        .attr("class", "links")
+        .selectAll("line")
+        .data(graph.links)
         .enter().append("line")
-        .attr("class", "link")
-        .style("stroke-width", function(d) { return Math.sqrt(d.weight); });
+        .attr("stroke-width", function(d) { return d.value; });
 
-    var node = svg.selectAll(".node")
-        .data(json.nodes)
-        .enter().append("g")
-        .attr("class", "node")
-        .call(force.drag);
+    var node = svg.append("g")
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(graph.nodes)
+        .enter().append("circle")
+        .attr("r", 5)
+        .attr("fill", function(d) { return color(d.group); })
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
 
-    node.append("circle")
-        .attr("r","5");
+    node.append("title")
+        .text(function(d) { return d.id; });
 
-    node.append("text")
-        .attr("dx", 12)
-        .attr("dy", ".35em")
-        .text(function(d) { return d.name });
+    simulation
+        .nodes(graph.nodes)
+        .on("tick", ticked);
 
-    force.on("tick", function() {
-        link.attr("x1", function(d) { return d.source.x; })
+    simulation.force("link")
+        .links(graph.links);
+
+    function ticked() {
+        link
+            .attr("x1", function(d) { return d.source.x; })
             .attr("y1", function(d) { return d.source.y; })
             .attr("x2", function(d) { return d.target.x; })
             .attr("y2", function(d) { return d.target.y; });
 
-        node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-    });
+        node
+            .attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+    }
 });
+
+function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+}
+
+function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+}
+
+function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+}
